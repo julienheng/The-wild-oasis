bbe0252f73438766a2645d9dfb4dfce2efb9c183
Julien <hengjchan@gmail.com>
2023-11-28 10:39:03 +0800

    setting up styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <H1>Hello World</H1>
+    </>
+  );
 }
 
 export default App;
