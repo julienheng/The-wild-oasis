912fd7230a93514a22c8185b38c69ca63b9f7512
Julien <hengjchan@gmail.com>
2023-11-28 13:51:55 +0800

    learning supabase

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,19 +1,32 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 //COMPONENTS
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
 export default function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
 
       {/*This is where the child routes will be rendered */}
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
