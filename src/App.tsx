19dcfe2b1eccf3d51c7545adb02402e08df43ccf
Julien <hengjchan@gmail.com>
2023-11-28 10:53:28 +0800

    setting up styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
