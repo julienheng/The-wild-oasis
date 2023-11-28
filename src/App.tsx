257335303c6c7e53957dd54f9edc6d30cc46b29e
Julien <hengjchan@gmail.com>
2023-11-28 12:16:14 +0800

    setting up styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,36 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <H1>Hello World</H1>
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <div>
+            <Heading as="h2">Check in and out</Heading>
+            <Button onClick={() => alert("Check in")}>Check in</Button>
+            <Button onClick={() => alert("Check out")}>Check out</Button>
+          </div>
+        </Row>
+
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </StyledApp>
     </>
   );
 }
