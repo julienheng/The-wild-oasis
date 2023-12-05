8d8ce0f2ac66475e90f6e893a3912e08d8945aff
Julien <hengjchan@gmail.com>
2023-12-05 17:11:19 +0800

    added dark mode

--- src/ui/ErrorFallback.tsx
+++ src/ui/ErrorFallback.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +33,24 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+type Props = {
+  error: any;
+  resetErrorBoundary: () => void;
+};
+
+export default function ErrorFallback({ error, resetErrorBoundary }: Props) {
+  return (
+    <GlobalStyles>
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </GlobalStyles>
+  );
+}
