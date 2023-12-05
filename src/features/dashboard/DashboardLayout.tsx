80e700730a5f0b0fbfd8880d3a471bd98398d012
Julien <hengjchan@gmail.com>
2023-12-05 11:34:16 +0800

    added dark mode

--- src/features/dashboard/DashboardLayout.tsx
+++ src/features/dashboard/DashboardLayout.tsx
@@ -1,4 +1,21 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+
+export default function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's Activity</div>
+      <div>Chart Stay Durations</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
 
 const StyledDashboardLayout = styled.div`
   display: grid;
